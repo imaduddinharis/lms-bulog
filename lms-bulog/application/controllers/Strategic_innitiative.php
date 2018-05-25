3e2ba0dc4f76ce77f3cac381a36878bc2986faa0
imaduddinharis <imaduddinhariss@gmail.com>
2018-05-25 18:05:40 +0700

    UPDATE: Parameter Strategic Innitiative (read)

---
+++ lms-bulog/application/controllers/Strategic_innitiative.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Admin
+ *
+ * @author NVM Team Creative
+ */
+class Strategic_innitiative extends CI_Controller {
+
+    var $assets;
+    var $API ="";
+	
+	public function __construct()
+
+	{
+		parent::__construct();
+        
+        //{{host}}
+        $this->API="https://api-bulog.sobatteknologi.xyz/";
+        
+        /*- load library -*/
+//		$this->load->library('template');
+        $this->load->library('session');
+        $this->load->library('curl');
+        
+        /*- load helper -*/
+		$this->load->helper('form');
+        $this->load->helper('url');
+        
+        /*- load template assets -*/
+		$this->assets     = base_url().'assets/';        
+	}
+	public function index()
+	{
+        /*- manage data throw -*/
+        $data['strategic_innitiative'] = json_decode($this->curl->simple_get($this->API.'strategic_innitiative'));
+        
+        /*- define reference assets -*/
+        $data['assets']=$this->assets;
+        $data['title']= 'Manage Strategic Innitiative';
+        
+        /*- define reference section view -*/
+		$data['head']=$this->load->view('template/head',$data, true);
+        $data['menu_profile']=$this->load->view('template/menu_profile',$data, true);
+        $data['container']=$this->load->view('content/parameter/strategic-innitiative-home',$data, true);
+        $data['content_sidebar']=$this->load->view('template/content-sidebar',$data, true);
+        $data['top_navigation']=$this->load->view('template/top_navigation',$data, true);
+        $data['foot']=$this->load->view('template/foot',$data, true);
+        
+		$this->load->view('template/index',$data);
+	}
+}
